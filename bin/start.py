b5f564e073f63430bb1a083b42c6ee57f2ce61ea
tomoemon <bach48+github_tomoemon@gmail.com>
2015-11-12 14:50:44 +0900

    initialize sys.path implicitly

--- bin/start.py
+++ bin/start.py
@@ -11,36 +11,36 @@ if sys.getdefaultencoding() == 'ascii':
   delattr(sys, 'setdefaultencoding')
 
 
-def init_path():
-    # monkeyrunner.bat から起動すると
-    # monkeyrunner.bat が存在するディレクトリをカレントディレクトリにしてしまうため、
-    # src ディレクトリからインポートできるようにする
-    os.chdir(os.getenv('CURRENT_DIR'))
-    sys.path.append(path.join(path.dirname(path.dirname(__file__)), "src"))
-    sys.path.append(path.join(path.dirname(path.dirname(__file__)), "lib"))
+# monkeyrunner.bat から起動すると
+# monkeyrunner.bat が存在するディレクトリをカレントディレクトリにしてしまうため、
+# src ディレクトリからインポートできるようにする
+os.chdir(os.getenv('CURRENT_DIR'))
+sys.path.append(path.join(path.dirname(path.dirname(__file__)), "src"))
+sys.path.append(path.join(path.dirname(path.dirname(__file__)), "lib"))
 
 
-def queueing(queue):
+def queueing(host, port, queue):
     import server
-    server.create_udp_server("127.0.0.1", 4000, queue)
+    server.create_udp_server(host, port, queue)
 
 
 def main():
-    init_path()
-
     from threading import Thread
     from Queue import Queue
     import command
+
+    HOST = "127.0.0.1"
+    PORT = 4000
 
     functions = dict([(i, getattr(command, i))
         for i in dir(command) if isinstance(getattr(command, i), types.FunctionType)])
 
     queue = Queue(32)
-    t = Thread(target=queueing, args=(queue,))
+    t = Thread(target=queueing, args=(HOST, PORT, queue))
     t.daemon = True
     t.start()
 
-    print("start serving: " + "127.0.0.1:4000")
+    print("start udp server: %s:%d" % (HOST, PORT)) # old-style formatting
 
     while True:
         q = queue.get()
@@ -48,7 +48,7 @@ def main():
         action = q['action']
         function = functions[action]
         params = q['params']
-        print("queue count: " + str(size))
+        print("queue count: %d" % size)
         print(q)
         function(**params)
 
